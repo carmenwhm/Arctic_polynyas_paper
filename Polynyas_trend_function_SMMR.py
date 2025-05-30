247b82fb8b554be1a6cb123dbe559743377aa5ac
Carmen Hau Man Wong <carmenwhm22@gmail.com>
2025-05-30 16:55:01 +0200

    Add files via upload

---
+++ Polynyas_trend_function_SMMR.py
@@ -0,0 +1,164 @@
+import os
+import glob
+import netCDF4 as nc 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import cartopy
+import cartopy.crs as ccrs
+from skimage.segmentation import flood, flood_fill
+from scipy.ndimage import label, generate_binary_structure, binary_fill_holes
+from scipy.interpolate import griddata
+from pyhdf.SD import *
+from pyhdf.HDF import *
+
+def nc_merge_files(data_folder,grid_folder,data_path_name,grid_path_name,nc_var_name):
+
+    full_lat_list = []
+    full_lon_list = []
+    full_nc_list = []
+    date_list = []
+
+    data_path = os.path.join(data_folder,'%s'%data_path_name)
+    nc_files = sorted(glob.glob(data_path))
+    grid_file = os.path.join(grid_folder,'%s'%grid_path_name)
+
+
+    for file in list(nc_files):
+        data_f = nc.Dataset(file)
+        file_var = data_f.variables['%s'%nc_var_name]
+        basename = os.path.basename(file)
+        date = os.path.splitext(basename)[0][:8]
+
+        full_nc_list.append(file_var)
+        date_list.append(date)
+
+    
+    grid_f = SD(grid_file, SDC.READ)
+    lat = grid_f.select('Latitudes').get()
+    lon = grid_f.select('Longitudes').get()
+
+    file_num = len(glob.glob(data_path))
+    full_lat_list = np.repeat(lat[np.newaxis,:,:],file_num,axis=0)
+    full_lon_list = np.repeat(lon[np.newaxis,:,:],file_num,axis=0)
+
+    full_lat_arr = np.array(full_lat_list)
+    full_lon_arr = np.array(full_lon_list)
+    full_nc_arr = np.array(full_nc_list)
+
+    print(full_lat_arr.shape)
+    print(full_nc_arr.shape)
+    print(file_num)
+
+    return full_lat_arr, full_lon_arr, full_nc_arr, date_list, file_num
+
+
+# Function for selecting specific region for studies based on lon lat 
+def select_region(full_lon,full_lat,full_var,lon_min,lon_max,lat_min,lat_max,Delete_data=None):
+    z1 = (full_lon > lon_min) & (full_lon < lon_max)
+    selected_lon = np.ma.masked_array(full_lon,mask=np.invert(z1))
+
+    z2 = (full_lat > lat_min) & (full_lat < lat_max)
+    selected_lat = np.ma.masked_array(full_lat,mask=np.invert(z2))
+
+    if Delete_data == True:                                                                                                         
+        region_mask = np.logical_and(z1,z2)
+        selected_var = full_var*region_mask
+        selected_var = np.where(region_mask,full_var,np.nan)
+
+    else:
+        region_mask = np.logical_and(z1,z2)
+        selected_var = np.ma.masked_array(full_var,mask=np.invert(region_mask))
+
+    return selected_lon,selected_lat,selected_var
+
+
+def select_region_rev(full_lon,full_lat,full_var,lon_min,lon_max,lat_min,lat_max,Delete_data=None):
+    z1 = (lon_min < full_lon) | (full_lon < lon_max)
+    selected_lon = np.ma.masked_array(full_lon,mask=np.invert(z1))
+
+    z2 = (full_lat > lat_min) & (full_lat < lat_max)
+    selected_lat = np.ma.masked_array(full_lat,mask=np.invert(z2))
+
+    if Delete_data == True:                                                           
+        region_mask = np.logical_and(z1,z2)
+        selected_var = full_var*region_mask
+        selected_var = np.where(region_mask,full_var,np.nan)
+
+    else:
+        region_mask = np.logical_and(z1,z2)
+        selected_var = np.ma.masked_array(full_var,mask=np.invert(region_mask))
+
+    return selected_lon,selected_lat,selected_var
+
+
+def nc_merge_files_NSIDC(data_folder,grid_folder,data_path_name,grid_name,nc_var_name):
+
+    data_path = os.path.join(data_folder,'%s'%data_path_name)
+    nc_files = sorted(glob.glob(data_path))
+    grid_file = os.path.join(grid_folder,'%s'%grid_name)
+
+    grid_f = nc.Dataset(grid_file)
+    upscale_lat = grid_f.variables['latitude'][:]
+    upscale_lon = grid_f.variables['longitude'][:]
+
+    full_lat_list = np.full((len(nc_files),upscale_lat.shape[0],upscale_lon.shape[1]),np.nan)
+    full_lon_list = np.full((len(nc_files),upscale_lat.shape[0],upscale_lon.shape[1]),np.nan)
+    full_nc_list = np.full((len(nc_files),upscale_lat.shape[0],upscale_lon.shape[1]),np.nan)
+    date_list = np.full(len(nc_files),np.nan)
+
+    for ifile,file in enumerate(nc_files):
+        try:
+            data_f = nc.Dataset(file)
+            file_var = data_f.variables['%s'%nc_var_name][0,:,:]
+            basename = os.path.basename(file)
+            date = basename.split('_')[4]
+
+            full_nc_list[ifile] = file_var
+            date_list[ifile] = date
+    
+        except KeyError:
+            try:
+                data_f = nc.Dataset(file)
+                file_var = data_f.variables['F11_ICECON'][0,:,:]
+                basename = os.path.basename(file)
+                date = basename.split('_')[4]
+    
+                full_nc_list[ifile] = file_var
+                date_list[ifile] = date
+
+            except KeyError:
+                try:
+                    data_f = nc.Dataset(file)
+                    file_var = data_f.variables['F17_ICECON'][0,:,:]
+                    basename = os.path.basename(file)
+                    date = basename.split('_')[4]
+        
+                    full_nc_list[ifile] = file_var
+                    date_list[ifile] = date
+
+                except KeyError:
+                    pass
+    
+    file_num = len(date_list)
+    full_lat_list = np.repeat(upscale_lat[np.newaxis,:,:],file_num,axis=0)
+    full_lon_list = np.repeat(upscale_lon[np.newaxis,:,:],file_num,axis=0)
+
+
+    print(full_lat_list.shape)
+    print(full_nc_list.shape)
+    print(file_num)
+
+    return full_lat_list, full_lon_list, full_nc_list, date_list, file_num
+
+
+def polynyas_loc_SIC_LR(SIC_arr,threshold):
+
+    time_SIC = np.nan_to_num(SIC_arr,nan=-999)
+    polynyas_loc = flood_fill(time_SIC,(124,50),999,tolerance=threshold)     ## This is 25 km seeding
+    polynyas_loc_1 = flood_fill(polynyas_loc,(405,230),999,tolerance=threshold) 
+    counts = np.logical_and(polynyas_loc_1>=0,polynyas_loc_1<=threshold)                                                                            
+    anchor_arr = counts.astype(int)
+    SIC_location = np.where(counts==1,1,0)
+
+    return polynyas_loc,polynyas_loc_1,SIC_location
---
+++ SMMR_polynya.py
@@ -0,0 +1,207 @@
+import netCDF4 as nc 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import cartopy
+import cartopy.crs as ccrs
+import matplotlib as mpl
+from skimage.segmentation import flood, flood_fill
+from scipy.ndimage import label, generate_binary_structure, binary_fill_holes
+from scipy.interpolate import griddata
+from pyhdf.SD import *
+from pyhdf.HDF import *
+from Polynyas_trend_function_SMMR import *
+
+
+if __name__ == '__main__':
+
+    # 25km SMMR SIC grid
+    f_read = nc.Dataset('./NSIDC0771_LatLon_PS_N25km_v1.0.nc')
+    SIC_lat_SMMR = f_read.variables['latitude'][:]
+    SIC_lon_SMMR = f_read.variables['longitude'][:]
+    
+    # 25km SMMR SIC grid area 
+    f_area = nc.Dataset('./NSIDC0771_CellArea_PS_N25km_v1.0.nc')
+    cell_area_SMMR = f_area['cell_area'][:]
+
+    # Year array
+    yr_1978 = np.arange(1978,2024)
+    
+    # Concentration variation
+    conc_SMMR = np.arange(0.3,0.7,0.1)
+
+    SIC_yearly_base_loc_1978 = np.zeros((conc_SMMR.shape[0],yr_1978.shape[0],SIC_lat_SMMR.shape[0],SIC_lat_SMMR.shape[1]))
+    Daily_location = np.full((SIC_lat_SMMR.shape[0],SIC_lat_SMMR.shape[1]),np.nan)
+    Daily_location = np.expand_dims(Daily_location, axis=0)
+    total_area_SIC = np.zeros((conc_SMMR.shape[0],yr_1978.shape[0]))
+    total_num_SIC = np.zeros((conc_SMMR.shape[0],yr_1978.shape[0]))
+    total_area_SMMR = np.zeros((conc_SMMR.shape[0],yr_1978.shape[0]))
+    date_list = []
+
+    # Read one winter files
+    for i, year in enumerate(yr_1978):
+        SIC_Data_folder = '/proj/naiss2024-22-523/PhD_Year1/1978_2024_SMMR/%s_%s/SIC/'%(year,year+1)
+        Grid_folder = '/proj/naiss2024-22-523/PhD_Year1/'
+        gridf_name_25 = 'NSIDC0771_LatLon_PS_N25km_v1.0.nc'
+        SIC_datafile_name = '*v2.0.nc' 
+    
+        if year in np.arange(1978,1987):
+            print(year)
+            full_lat_arr, full_lon_arr, full_sic_arr, date_test, siconc_file_num = nc_merge_files_NSIDC(SIC_Data_folder,Grid_folder,\
+                                                                                        SIC_datafile_name,gridf_name_25,'N07_ICECON')
+            
+        elif year in np.arange(1987,1992):
+            print(year)
+            full_lat_arr, full_lon_arr, full_sic_arr, date_test, siconc_file_num = nc_merge_files_NSIDC(SIC_Data_folder,Grid_folder,\
+                                                                                        SIC_datafile_name,gridf_name_25,'F08_ICECON')
+            
+        elif year in np.arange(1992,1995):
+            print(year)
+            full_lat_arr, full_lon_arr, full_sic_arr, date_test, siconc_file_num = nc_merge_files_NSIDC(SIC_Data_folder,Grid_folder,\
+                                                                                        SIC_datafile_name,gridf_name_25,'F11_ICECON')
+            
+        elif year in np.arange(1995,2008):
+            print(year)
+            full_lat_arr, full_lon_arr, full_sic_arr, date_test, siconc_file_num = nc_merge_files_NSIDC(SIC_Data_folder,Grid_folder,\
+                                                                                        SIC_datafile_name,gridf_name_25,'F13_ICECON')
+        else:
+            print(year)
+            full_lat_arr, full_lon_arr, full_sic_arr, date_test, siconc_file_num = nc_merge_files_NSIDC(SIC_Data_folder,Grid_folder,\
+                                                                                        SIC_datafile_name,gridf_name_25,'F17_ICECON')
+    
+        date_list.append(date_test)
+        
+        SIC_location = np.zeros((siconc_file_num,SIC_lat_SMMR.shape[0],SIC_lat_SMMR.shape[1]))     
+        SIC_polynyas_labeled = np.zeros((siconc_file_num,SIC_lat_SMMR.shape[0],SIC_lat_SMMR.shape[1]))
+        filter_sic = np.zeros((siconc_file_num,SIC_lat_SMMR.shape[0],SIC_lat_SMMR.shape[1]))
+        SIC_polynyas_num = np.zeros(siconc_file_num)
+
+        for j, concentration in enumerate(conc_SMMR):
+            for k in range(siconc_file_num):
+
+                SIC_area = []
+                SIC_daily_num = []
+    
+                index = 0+k
+                
+                masked_SIC = np.where(full_sic_arr[index]>250,-999,full_sic_arr[index])
+                _, _, SIC_location[index] = polynyas_loc_SIC_LR(masked_SIC,concentration)
+    
+                _, _, exclude_regional_1 = select_region(full_lon=SIC_lon_SMMR,full_lat=SIC_lat_SMMR,full_var=full_sic_arr[index],\
+                                                                        lon_min=70,lon_max=84,lat_min=66,lat_max=73,Delete_data=True)
+        
+                _, _, exclude_regional_2 = select_region(full_lon=SIC_lon_SMMR,full_lat=SIC_lat_SMMR,full_var=full_sic_arr[index],\
+                                                                                lon_min=20,lon_max=25,lat_min=65,lat_max=66,Delete_data=True)
+                
+                _, _, exclude_regional_3 = select_region_rev(full_lon=SIC_lon_SMMR,full_lat=SIC_lat_SMMR,full_var=full_sic_arr[index],\
+                                                                                lon_min=180,lon_max=-175,lat_min=65,lat_max=67,Delete_data=True)
+                
+                _, _, exclude_regional_4 = select_region(full_lon=SIC_lon_SMMR,full_lat=SIC_lat_SMMR,full_var=full_sic_arr[index],\
+                                                                                lon_min=-68,lon_max=-64,lat_min=65,lat_max=66.5,Delete_data=True)
+                
+                _, _, exclude_regional_5 = select_region(full_lon=SIC_lon_SMMR,full_lat=SIC_lat_SMMR,full_var=full_sic_arr[index],\
+                                                                                lon_min=-125,lon_max=-117,lat_min=65,lat_max=67,Delete_data=True)
+    
+                _, _, exclude_regional_6 = select_region(full_lon=SIC_lon_SMMR,full_lat=SIC_lat_SMMR,full_var=full_sic_arr[index],\
+                                                                                lon_min=33,lon_max=45,lat_min=65,lat_max=67.5,Delete_data=True)
+            
+                all_exclude_regional = np.isfinite(exclude_regional_1).astype(int) + np.isfinite(exclude_regional_2).astype(int) + \
+                                       np.isfinite(exclude_regional_3).astype(int) + np.isfinite(exclude_regional_4).astype(int) + \
+                                       np.isfinite(exclude_regional_5).astype(int) + np.isfinite(exclude_regional_6).astype(int)
+    
+                filter_region = np.where(all_exclude_regional>0,np.nan,SIC_location[index])
+                filter_masked_SIC = np.where(all_exclude_regional>0,np.nan,masked_SIC)
+    
+                _, _, SIC_polynyas_labeled[index] = select_region(full_lon=SIC_lon_SMMR,full_lat=SIC_lat_SMMR,full_var=filter_region,\
+                                                                            lon_min=-180,lon_max=180,lat_min=65,lat_max=90,Delete_data=True)
+    
+
+                for_label_arr = np.nan_to_num(SIC_polynyas_labeled[index], nan=0)
+                s = generate_binary_structure(2,2)
+                daily_arr, daily_num = label(for_label_arr,structure=s)
+                SIC_daily_num.append(daily_num)
+                
+                daily_area_sic = np.sum(cell_area_SMMR[SIC_polynyas_labeled[index]>0])/(1000**2)
+                SIC_area.append(daily_area_sic)              
+                
+                for idx, _ in enumerate(full_sic_arr):
+                    if np.any(np.isfinite(full_sic_arr[idx])==False):
+                        SIC_polynyas_labeled[idx] = np.nan
+            
+            Daily_location = np.concatenate((Daily_location,SIC_polynyas_labeled),axis=0)
+
+            SIC_winterly_area = np.sum(SIC_area)
+            SIC_base_loc = np.sum(SIC_polynyas_labeled,axis=0)>0
+    
+            s = generate_binary_structure(2,2)
+            SIC_base_loc_arr, SIC_base_num = label(SIC_base_loc,structure=s)
+    
+            SIC_yearly_base_loc_1978[j,i] = SIC_base_loc
+            total_num_SIC[j,i] = np.sum(SIC_daily_num)
+            total_area_SIC[j,i] = SIC_winterly_area
+            total_area_SMMR[j,i] = np.sum(cell_area_SMMR[SIC_base_loc_arr>0])/(1000**2)
+
+
+    # Save polynya winter base map
+    pathsave = '/proj/naiss2024-22-523/PhD_Year1/20250430/Results/'
+    
+    with nc.Dataset(f'{pathsave}SMMR_winter_basemap.nc', 'w', format='NETCDF4') as file: \
+        # Create dimensions
+        file.createDimension('conc', conc_SMMR.shape[0])
+        file.createDimension('year', yr_1978.shape[0])
+        file.createDimension('y', SIC_lat_SMMR.shape[0])
+        file.createDimension('x', SIC_lat_SMMR.shape[1])
+        
+        time_var = file.createVariable('year', 'f4', ('year',))
+        time_var[:] = yr_1978
+    
+        lat_var = file.createVariable('lat','f4',('y','x'))
+        lat_var[:] = SIC_lat_SMMR 
+        
+        lon_var = file.createVariable('lon','f4',('y','x'))
+        lon_var[:] = SIC_lon_SMMR
+    
+        SIC_winter_loc = file.createVariable('SIC_winter_loc','f4',('conc','year','y','x'))
+        SIC_winter_loc[:] = SIC_yearly_base_loc_1978[:]
+
+    # Generate date array
+    import itertools
+    flatten_date = list(itertools.chain.from_iterable(date_list))
+    time_series = pd.Series(flatten_date)
+    formatted_dates = pd.to_datetime(time_series.astype('Int64').astype(str),format='%Y%m%d',errors='coerce')
+    formatted_dates_array = formatted_dates.dt.strftime('%Y-%m-%d').to_numpy()
+
+    # Save daily polynya locations
+    pathsave = '/proj/naiss2024-22-523/PhD_Year1/20250430/Results/'
+    with nc.Dataset(f'{pathsave}daily_location_SMMR.nc', 'w', format='NETCDF4') as file: \
+        # Create dimensions
+        file.createDimension('time', Daily_sic.shape[0] - 1)
+        file.createDimension('y', Daily_sic.shape[1])
+        file.createDimension('x', Daily_sic.shape[2])
+        
+        time_var = file.createVariable('time', str, ('time',))
+        time_var[:] = formatted_dates_array.astype(np.bytes_)
+    
+        lat_var = file.createVariable('lat','f4',('y','x'))
+        lat_var[:] = SIC_lat_SMMR 
+        
+        lon_var = file.createVariable('lon','f4',('y','x'))
+        lon_var[:] = SIC_lon_SMMR 
+    
+        polynyas_loc_var = file.createVariable('polynyas_loc','f4',('time','y','x'))
+        polynyas_loc_var[:] = Daily_location[1:,:,:] 
+    
+    # Save cumulative number, total area and cumulative area
+    period_list = []
+    
+    for year in yr_1978:
+        period_list.append('%s-%s'%(year,year+1))
+    
+    area_table = pd.DataFrame(data=total_area_SIC,index=conc_SMMR,columns=period_list)
+    area_table_2 = pd.DataFrame(data=total_area_SMMR,index=conc_SMMR,columns=period_list)
+    num_table = pd.DataFrame(data=total_num_SIC,index=conc_SMMR,columns=period_list)
+    
+    area_table.to_csv('/proj/naiss2024-22-523/PhD_Year1/20250430/Area_csv/Arctic_cumulative_area_1978_2024_SMMR.csv',sep=',')
+    area_table_2.to_csv('/proj/naiss2024-22-523/PhD_Year1/20250430/Area_csv/Arctic_potential_area_1978_2024_SMMR.csv',sep=',')
+    num_table.to_csv('/proj/naiss2024-22-523/PhD_Year1/20250430/Area_csv/Arctic_cumulative_num_1978_2024_SMMR.csv',sep=',')
+
